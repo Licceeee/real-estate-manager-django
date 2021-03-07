6bf38f4f29c4904ff5480233c93b9ecd02437058
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-03-07 12:37:35 +0100

    undo

--- scripts/clean_migrations.sh
+++ scripts/clean_migrations.sh
@@ -11,19 +11,19 @@ python manage.py migrate auth zero
 python manage.py migrate contenttypes zero
 python manage.py migrate sessions zero
 
-python3 manage.py migrate
+# python3 manage.py migrate
 
-python3 manage.py makemigrations core
+# python3 manage.py makemigrations core
 
-python3 manage.py makemigrations documents
+# python3 manage.py makemigrations documents
 
-python3 manage.py makemigrations listings
+# python3 manage.py makemigrations listings
 
-python3 manage.py makemigrations contacts
+# python3 manage.py makemigrations contacts
 
-python3 manage.py makemigrations accounts
+# python3 manage.py makemigrations accounts
 
-python3 manage.py migrate
+# python3 manage.py migrate
 
 
 
